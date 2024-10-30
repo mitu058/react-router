9e6215e8fa7ff5f221631a9250546a4738da60e7
mitu058 <mitum9796@gmail.com>
2024-10-31 00:08:10 +0600

    setup react router

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>contact us</h2>
+            <p>we are call a way</p>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Nabar</h2>
+            <nav>
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact us">Contact us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
