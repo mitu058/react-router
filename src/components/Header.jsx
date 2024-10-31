99607888d58d4bf31b42c3777b2d19872f938e28
mitu058 <mitum9796@gmail.com>
2024-10-31 14:38:14 +0600

    show active route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
             <h2>Nabar</h2>
             <nav>
-                <Link className="mr-3" to="/">Home</Link>
-                <Link className="mr-3" to="/about">About</Link>
-                <Link className="mr-3" to="/users">Users</Link>
-                <Link className="mr-3" to="/posts">Posts</Link>
-                <Link className="mr-3" to="/contact">Contact</Link>
+                <NavLink className="mr-3" to="/">Home</NavLink>
+                <NavLink className="mr-3" to="/about">About</NavLink>
+                <NavLink className="mr-3 " to="/users">Users</NavLink>
+                <NavLink className="mr-3" to="/posts">Posts</NavLink>
+                <NavLink className="mr-3" to="/contact">Contact</NavLink>
                
             </nav>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Home = () => {
+    const navigation = useNavigation()
     return (
         <div>
             <Header></Header>
